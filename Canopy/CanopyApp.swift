f9816257d7ec79a45f6d9f51983b25641851b1b3
Doug Green <douglasrogergreen@gmail.com>
2026-05-21 15:34:47 -0800

    Initial Commit

---
+++ Canopy/CanopyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CanopyApp.swift
+//  Canopy
+//
+//  Created by Doug Green on 5/21/26.
+//
+
+import SwiftUI
+
+@main
+struct CanopyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
